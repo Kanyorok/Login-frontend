ba217c15b0279392c2a851b298bd353bc748ac8b
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2024-02-17 20:29:48 +0300

    Add the design for the dashboard page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 export default class Profile extends Component {
   render() {
     return (
-      <div>
-        <h1>Awesome Profile</h1>
+      <div className="flex w-full justify-center">
+        <h1 className="text-lg text-white mt-10 p-5 rounded-lg bg-black">Awesome Profile</h1>
       </div>
     )
   }
